2b05c98b58a302de224ea024f1447fef4e017ab6
Anitha-Arjun <arjunmurugan@Arjuns-MacBook-Air.local>
2024-09-17 20:54:58 -0500

    first commit

---
+++ src/components/Score.jsx
@@ -0,0 +1,16 @@
+
+
+
+
+
+export default function Score(props) {
+    return (
+      <div>
+        <div>Score: {props.score.score} | Date: {props.score.date}</div>
+      </div>
+    );
+  }
+
+
+
+
